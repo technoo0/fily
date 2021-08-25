51b73a419f298ce54a046f460766ac959ae1ef17
technoo0 <marwanmf2050@gmail.com>
2021-08-25 22:51:09 +0200

    finishes

--- Backend/config/database.js
+++ Backend/config/database.js
@@ -7,7 +7,10 @@ if (process.env.NODE_ENV == "production") {
       dialect: "postgres",
       protocol: "postgres",
       dialectOptions: {
-        ssl: true,
+        ssl: {
+          require: true,
+          rejectUnauthorized: false,
+        },
       },
     });
   } catch (e) {
